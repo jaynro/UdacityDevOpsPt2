bcfd88afe9243b211882aed7be4660db45f0bf5b
jaynro <jaynro@gmail.com>
2021-01-18 08:22:16 -0600

    adding performance

---
+++ locustfile.py
@@ -0,0 +1,28 @@
+import time
+from locust import HttpUser, task
+
+
+class WebsiteUser(HttpUser):
+    
+    
+    @task
+    def makepredict(self):
+        self.client.post('/predict', json={"CHAS":{  
+          "0":0.0
+       },
+       "RM":{  
+          "0":6.575
+       },
+       "TAX":{  
+          "0":296.0
+       },
+       "PTRATIO":{  
+          "0":15.3
+       },
+       "B":{  
+          "0":396.9
+       },
+       "LSTAT":{  
+          "0":4.98
+       }})
+
